3d0abffb5357d1454acede6aac9ccd2f0e1da8a4
danakristiawan <danakristiawan@gmail.com>
2023-07-26 12:01:44 +0700

    build facade for datatable

---
+++ resources/views/users/action.blade.php
@@ -0,0 +1,4 @@
+<div class="btn-group">
+  <a href="#" class="btn btn-sm btn-outline-success"><i class="bi bi-pencil"></i> Edit</a>
+  <a href="#" class="btn btn-sm btn-outline-success"><i class="bi bi-trash"></i> Delete</a>
+</div>
