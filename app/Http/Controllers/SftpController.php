47b89d96a2248a9bc552708b90642544f1b775e5
danakristiawan <danakristiawan@gmail.com>
2023-08-10 09:55:03 +0700

    adding file txt from sftp mandiri

--- app/Http/Controllers/SftpController.php
+++ app/Http/Controllers/SftpController.php
@@ -20,26 +20,11 @@ class SftpController extends Controller
 
     public function show($file)
     {
-        // $contents = File::get($get);
         $contents = Storage::disk('sftp')->get($file);
-        // $lines = explode("\n", $contents);
-        // $collection = collect();
-        // foreach ($lines as $line) {
-        //     $report = explode(':', $line);
-        //     if (isset($report[1]) && isset($report[2])) {
-        //         if ($report[1] === '61' || $report[1] === '86') {
-        //             $object = new stdClass();
-        //             $object->code = $report[1];
-        //             $object->detail = $report[2];
-        //             $collection->push($object);
-        //         }
-        //     }
-        //     // array_push($array, $report);
-        // }
-
         $lines = explode("\n", $contents);
         $col1 = collect();
         $col2 = collect();
+        $cols = collect();
         foreach ($lines as $line) {
             $report = explode(':', $line);
             if (isset($report[1]) && isset($report[2])) {
@@ -55,7 +40,6 @@ class SftpController extends Controller
                 }
             }
         }
-        $cols = collect();
         $i = 0;
         foreach($col1 as $item) {
             $object = new stdClass();
@@ -64,8 +48,6 @@ class SftpController extends Controller
             $cols->push($object);
             $i++;
         }
-
-        // dd($cols);
 
         return view('sftp.show', compact('cols'));
     }
@@ -74,33 +56,6 @@ class SftpController extends Controller
     {
         $contents = File::get('storage/202307270617-DJKN-MT940_H1-0010541288.csv');
 
-        // $collection1 = collect();
-        // $collection2 = collect();
-        // $lines = explode("\n", $contents);
-        // foreach($lines as $line) {
-        //     if(substr($line, 0,3)===':61') {
-        //         $object = new stdClass();
-        //         $object = $line;
-        //         $collection1->push($object);
-        //     }
-        //     if(substr($line, 0,3)===':86') {
-        //         $object = new stdClass();
-        //         $object = $line;
-        //         $collection2->push($object);
-        //     }
-        // }
-        
-        // $cols1 = collect();
-        // $i = 0;
-        // foreach($collection1 as $col1) {
-        //     $object = new stdClass();
-        //     $object = $col1 . ' '. $collection2[$i];
-        //     $cols1->push($object);
-        //     $i++;
-        // }
-
-        // return dd($cols1);
-
         $lines = explode("\n", $contents);
         $col1 = collect();
         $col2 = collect();
@@ -128,8 +83,6 @@ class SftpController extends Controller
             $cols->push($object);
             $i++;
         }
-
-        // dd($cols);
 
         return view('sftp.tes2', compact('cols'));
 
